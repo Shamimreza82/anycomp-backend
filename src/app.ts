5e80265dfe5d6f33eaf66159a6771392c7ae37d9
Shamim Reza <shamimrezaone@gmail.com>
2026-02-07 12:30:40 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,15 @@ app.use(cookieParser());
 // Middleware
 
 
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// )
+
+
+
 app.use(
   cors({
     origin: "https://anycomp-frontend-eight.vercel.app",
